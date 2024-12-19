900e550de4360c8daf0286d44ec3b6b85baeff03
dyxgou <diego5614yt@gmail.com>
2024-12-19 10:55:32 -0500

    feat(repl): Implemented REPL for redis

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "log/slog"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const prompt = "redis> "
 
 func main() {
-	slog.Info("client")
+	conn, err := net.Dial("tcp", "localhost:5000")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(prompt)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		text := scanner.Text()
+
+		_, err := conn.Write([]byte(text))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
