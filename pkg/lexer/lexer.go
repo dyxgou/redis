b94b250b8189039b96d96cd447718e83212b0370
dyxgou <diego5614yt@gmail.com>
2024-12-19 10:58:48 -0500

    feat(lexer): Commamnd's grammar implemented

---
+++ pkg/lexer/lexer.go
@@ -0,0 +1,128 @@
+package lexer
+
+import (
+	"fmt"
+	"github/dyxgou/redis/pkg/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+
+	l.next()
+
+	return l
+}
+
+func (l *Lexer) next() {
+	if l.readPosition >= len(l.input) {
+		l.ch = byte(token.EOF)
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '+':
+		return token.New(token.STRING, string(l.ch))
+	case '-':
+		return token.New(token.ERROR, string(l.ch))
+	case ':':
+		return token.New(token.INTEGER, string(l.ch))
+	case '$':
+		return token.New(token.BULKSTRING, string(l.ch))
+	case '*':
+		return token.New(token.ARRAY, string(l.ch))
+	case '_':
+		return token.New(token.NULL, string(l.ch))
+	case '#':
+		return token.New(token.BOOLEAN, string(l.ch))
+	case ',':
+		return token.New(token.FLOAT, string(l.ch))
+	case '(':
+		return token.New(token.BIGNUMBER, string(l.ch))
+	case '!':
+		return token.New(token.BULKERROR, string(l.ch))
+	case '=':
+		return token.New(token.VERTAMINSTRING, string(l.ch))
+	case '%':
+		return token.New(token.MAPS, string(l.ch))
+	case '`':
+		return token.New(token.ATTRIBUTES, string(l.ch))
+	case '~':
+		return token.New(token.SET, string(l.ch))
+	case '>':
+		return token.New(token.PUSHES, string(l.ch))
+	}
+
+	var t token.Token
+
+	if isLetter(l.ch) {
+		t.Literal = l.readIdentifier()
+		t.Kind = token.LookupIdent(t.Literal)
+	} else if isDigit(l.ch) {
+		literal, err := l.readNumber()
+		t.Literal = literal
+
+		if err != nil {
+			t.Kind = token.ILEGAL
+		}
+	} else {
+		t = token.New(token.ILEGAL, string(l.ch))
+	}
+
+	l.next()
+	return t
+}
+
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return (ch >= '1' && ch <= '9') || ch == '.'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == '_' || l.ch == '\n' || l.ch == '\t' || l.ch == '\r' {
+		l.next()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	pos := l.position
+
+	for isLetter(l.ch) {
+		l.next()
+	}
+
+	return l.input[pos:l.position]
+}
+
+func (l *Lexer) readNumber() (string, error) {
+	pos := l.position
+
+	for isDigit(l.ch) {
+		l.next()
+	}
+
+	number := l.input[pos:l.position]
+
+	if l.input[l.position-1] == '.' {
+		return number, fmt.Errorf("un expected '.' at the end of the number.")
+	}
+
+	return number, nil
+}
---
+++ pkg/lexer/lexer_test.go
@@ -0,0 +1,76 @@
+package lexer
+
+import (
+	"github/dyxgou/redis/pkg/token"
+	"testing"
+)
+
+func TestInputToken(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected token.Token
+	}{
+		{"+", token.New(token.STRING, "+")},
+		{"-", token.New(token.ERROR, "-")},
+		{":", token.New(token.INTEGER, ":")},
+		{"GET", token.New(token.GET, "GET")},
+		{"GETSET", token.New(token.GETSET, "GETSET")},
+		{"SET", token.New(token.SET, "SET")},
+	}
+
+	for _, tt := range tests {
+		l := New(tt.input)
+
+		tok := l.NextToken()
+
+		if tok.Literal != tt.expected.Literal {
+			t.Errorf("tok literal expected=%s. got=%s", tt.expected.Literal, tok.Literal)
+		}
+
+		if tok.Kind != tt.expected.Kind {
+			t.Errorf("tok kind expected=%d. got=%d", tt.expected.Kind, tok.Kind)
+		}
+	}
+}
+
+func TestIlegalToken(t *testing.T) {
+	input := "@^&)'"
+
+	l := New(input)
+
+	for l.ch != byte(token.EOF) {
+		tok := l.NextToken()
+
+		if tok.Kind != token.ILEGAL {
+			t.Errorf("token kind ILEGAL expected=%d, got=%d (%s)", token.ILEGAL, tok.Kind, tok.Literal)
+		}
+	}
+}
+
+func TestCommandTokenized(t *testing.T) {
+	test := struct {
+		input          string
+		expectedTokens []token.Token
+	}{
+		input: "GET my_key my_value",
+		expectedTokens: []token.Token{
+			{Kind: token.GET, Literal: "GET"},
+			{Kind: token.IDENT, Literal: "my_key"},
+			{Kind: token.IDENT, Literal: "my_value"},
+		},
+	}
+
+	l := New(test.input)
+
+	for _, tok := range test.expectedTokens {
+		cur := l.NextToken()
+
+		if cur.Kind != tok.Kind {
+			t.Errorf("tok kind expected=%d. got=%d", tok.Kind, cur.Kind)
+		}
+
+		if cur.Literal != tok.Literal {
+			t.Errorf("tok literal expected=%s. got=%s", tok.Literal, cur.Literal)
+		}
+	}
+}
