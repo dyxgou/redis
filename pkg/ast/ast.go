2cd13b2522b2a0f1c63900e507b763958e96add4
dyxgou <diego5614yt@gmail.com>
2024-12-28 21:56:03 -0500

    fix(ast) CommandReader removed

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -1,45 +1,19 @@
 package ast
 
-import "strings"
-
+// Command nodes represent each command read from a connection
 type Node interface {
 	String() string
 	TokenLiteral() string
 }
 
-// Command nodes represent each command read from a connection
 type Command interface {
 	Node
 
-	commandNode()
+	cmdNode()
 }
 
-// CommandReader is the reader we are gonna use in each connection which is gonna transfrom the string input into an AST.
-type CommandReader struct {
-	Commands []Command
-}
+type Expression interface {
+	Node
 
-// NewCommandReader() creates a new *CommandReader with a default lenght of 20
-func NewCommandReader() *CommandReader {
-	return &CommandReader{
-		Commands: make([]Command, 0, 20),
-	}
-}
-
-func (cr *CommandReader) String() string {
-	var sb strings.Builder
-
-	for _, cmd := range cr.Commands {
-		sb.WriteString(cmd.String())
-	}
-
-	return sb.String()
-}
-
-func (cr *CommandReader) TokenLiteral() string {
-	if len(cr.Commands) == 0 {
-		return ""
-	}
-
-	return cr.Commands[0].TokenLiteral()
+	exprNode()
 }
