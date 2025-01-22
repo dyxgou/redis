5b33e405c46da7235e02d13336e0fbabe6377a0e
dyxgou <diego5614yt@gmail.com>
2025-01-21 21:59:39 -0500

    add(storage): String method to value fields

--- internal/storage/values.go
+++ internal/storage/values.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"errors"
 	"github/dyxgou/redis/pkg/ast"
+	"strconv"
 )
 
 var NotSupportedValErr = errors.New("value provided not supported")
@@ -25,6 +26,7 @@ const (
 
 type Value interface {
 	kind() valueKind
+	String() string
 }
 
 func NewValue(v ast.Expression) Value {
@@ -74,3 +76,16 @@ func (b *Bool) kind() valueKind   { return boolKind }
 func (f *Float) kind() valueKind  { return floatKind }
 func (i *Int64) kind() valueKind  { return int64Kind }
 func (s *String) kind() valueKind { return stringKind }
+
+func (n *NilVal) String() string { return "(nil)" }
+func (i *Int) String() string    { return strconv.Itoa(i.Value) }
+func (f *Float) String() string  { return strconv.FormatFloat(f.Value, 'E', -1, 64) }
+func (i *Int64) String() string  { return strconv.FormatInt(i.Value, 10) }
+func (s *String) String() string { return s.Value }
+func (b *Bool) String() string {
+	if b.Value {
+		return "true"
+	} else {
+		return "false"
+	}
+}
