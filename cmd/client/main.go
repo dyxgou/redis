aef63bf9e9b1ed3fead6877d46ef7961bbf991d9
dyxgou <diego5614yt@gmail.com>
2025-02-19 18:22:38 -0500

    add(client): When Ctrl+C is pressed it closes the conneciton

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,14 +5,32 @@ import (
 	"fmt"
 	"github/dyxgou/redis/pkg/serializer"
 	"log"
+	"log/slog"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 const prompt = "redis> "
 
 func main() {
 	conn, err := net.Dial("tcp", "localhost:5000")
+
+	quitch := make(chan os.Signal, 1)
+	signal.Notify(quitch, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		select {
+		case <-quitch:
+			fmt.Println()
+			slog.Info("redis client", "exit", conn.RemoteAddr())
+			if err := conn.Close(); err != nil {
+				slog.Error("closing conn", "err", err)
+			}
+			os.Exit(0)
+		}
+	}()
 
 	if err != nil {
 		log.Fatal(err)
@@ -44,6 +62,15 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		resBuf := make([]byte, 1024)
+		n, err := conn.Read(resBuf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		msg := string(resBuf[:n])
+		fmt.Printf("server> %s\n", msg)
 	}
 }
 
